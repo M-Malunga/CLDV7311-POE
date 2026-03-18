bd53fae50ae869b26d472ef27a621ae4f5016ad7
Mmangaliso <st10296771@rcconnect.edu.za>
2026-03-18 22:56:31 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10296771_CLDV7311_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
